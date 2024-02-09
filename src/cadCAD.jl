d629209e20e16586e4f6ac90ca1b1630148e4d77
Emanuel Lima <emanuel-lima@outlook.com>
2024-02-09 17:11:38 +0100

    Initial Space experiment
    Signed-off-by: Emanuel Lima <emanuel-lima@outlook.com>

--- src/cadCAD.jl
+++ src/cadCAD.jl
@@ -1,5 +1,19 @@
 module cadCAD
 
-# Write your package code here.
+include("spaces.jl")
 
+using .Spaces
+
+@space begin
+    myname::String
+    age::Int64
+end
+
+#println(fieldnames(State))
+
+A = @NamedTuple begin
+    a::Float64
+    b::String
+end
+println(fieldnames(A))
 end
