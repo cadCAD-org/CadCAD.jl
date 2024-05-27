c5c52d974bd4d84b543d2c464430f4d05a7684bc
Emanuel Lima <emanuel-lima@outlook.com>
2024-05-27 10:25:01 -0300

    Compositions scaffold
    Signed-off-by: Emanuel Lima <emanuel-lima@outlook.com>

--- src/compositions.jl
+++ src/compositions.jl
@@ -1,6 +1,10 @@
+module Compositions
+
 import Base: >
 
 # Must overload > to broadcast NamedTuples into functions
 function >(prev_block_result::NamedTuple{Vararg{DataType}}, block2::T) where {T<:Function}
     .
+end
+
 end
